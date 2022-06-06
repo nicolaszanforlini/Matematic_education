e1ade42adeae2f29ab0e2853c8210b55c6123649
nicolaszanforlini <nicolas.zanforlini1@gmail.com>
2022-06-06 09:18:14 +0200

    [HomeFragment] create Fragment for home activity
    - HomeAdapter.java
    - HomeFragment.java
    - fragment_home.xml
    - line_adapter_home.xml

--- app/src/main/java/com/example/matematic/MainActivity.java
+++ app/src/main/java/com/example/matematic/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.matematic.fragments.HomeFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HomeFragment frag = new HomeFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.frame, frag).commit();
+
     }
 }
---
+++ app/src/main/java/com/example/matematic/adapters/HomeAdapter.java
@@ -0,0 +1,54 @@
+package com.example.matematic.adapters;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import com.example.matematic.R;
+import com.google.android.material.button.MaterialButton;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeAdapter extends BaseAdapter {
+
+    private List<String> listThemesTitles = new ArrayList<>();
+    private LayoutInflater themesInf;
+    private Context context;
+
+    public HomeAdapter(Context c, List<String>l) {
+        this.listThemesTitles = l;
+        this.context = c;
+        this.themesInf=LayoutInflater.from( c );
+    }
+
+    @Override
+    public int getCount() {
+        return this.listThemesTitles.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        @SuppressLint("ViewHolder") LinearLayout themesLay = (LinearLayout) this.themesInf.inflate
+                (R.layout.line_adapter_home, viewGroup, false);
+
+        MaterialButton btn = themesLay.findViewById(R.id.btnTitle);
+        String title = this.listThemesTitles.get(i);
+        btn.setText(title);
+
+        themesLay.setTag(i);
+        return themesLay;
+    }
+}
